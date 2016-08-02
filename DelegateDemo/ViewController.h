382914462527e0513c840e64d7390bb1aede8ef3
Cloudox <cloudox@foxmail.com>
2016-08-02 16:55:07 +0800

    Initial Commit

---
+++ DelegateDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DelegateDemo
+//
+//  Created by csdc-iMac on 16/8/2.
+//  Copyright © 2016年 Cloudox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
